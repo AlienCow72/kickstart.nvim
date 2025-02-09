c194d1587041e3abfaebf881822c66cdcb9b0a23
Kyle Anderson <kyle.anderson@dstax.com>
2025-02-08 21:59:43 -0600

    somthing

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    'github/copilot.vim',
+    config = function()
+        -- Optionally add any copilot-specific configurations here
+    end,
+}
